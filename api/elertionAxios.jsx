e977e8235ad296a00ce3d5a5c91e7430c8578fad
梁永琪 <liangyongqi@yongqi.local>
2024-06-29 21:05:37 +0800

    up

--- api/elertionAxios.jsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-import { webSite } from './webSite'
-
-const api = axios.create({
-    baseURL: webSite,
-    timeout: 20000, // 请求超时时间 当请求时间超过20秒还未取得结果时 提示用户请求超时
-})
-
-export default api
--- babel.config.js
+++
@@ -1,8 +0,0 @@
-// babel.config.js  
-module.exports = {  
-    presets: [  
-      ['@babel/preset-env', { targets: "> 0.25%, not dead" }], // 根据你的需要配置 targets  
-      '@babel/preset-react',  
-    ],  
-    // 添加其他 Babel 插件或选项...  
-  };
